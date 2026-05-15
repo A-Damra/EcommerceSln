cb3847a3cf1e1e03a297a594b64df1fea8849fa9
damra <damra@DAMRA>
2026-05-15 20:17:35 +0300

    initial front

--- Ecommerce.FrontEnd/Program.cs
+++ Ecommerce.FrontEnd/Program.cs
@@ -8,4 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped<Ecommerce.FrontEnd.Services.ApiAccess.ApiClient>();
+
 await builder.Build().RunAsync();
