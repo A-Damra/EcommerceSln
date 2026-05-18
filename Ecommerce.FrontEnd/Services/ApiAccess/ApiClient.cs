fcbe442ce3bc7ca2a5ff043bad9c9e467fb58b73
damra <damra@DAMRA>
2026-05-18 16:27:48 +0300

    api-fixes

--- Ecommerce.FrontEnd/Services/ApiAccess/ApiClient.cs
+++ Ecommerce.FrontEnd/Services/ApiAccess/ApiClient.cs
@@ -13,12 +13,26 @@ namespace Ecommerce.FrontEnd.Services.ApiAccess
 
         public async Task<T?> GetAsync<T>(string url)
         {
-            return await httpClient.GetFromJsonAsync<T>("https://localhost:7039/" + url);
+            return await httpClient.GetFromJsonAsync<T>("http://localhost:5130/" + url);
         }
 
         public async Task<TResponse?> PostAsync<TRequest, TResponse>(string url, TRequest data)
         {
-            var response = await httpClient.PostAsJsonAsync(url, data);
+            var response = await httpClient.PostAsJsonAsync("http://localhost:5130/" + url, data);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<TResponse>();
+        }
+
+        public async Task<TResponse?> Delete<TResponse>(string url, Guid id)
+        {
+            var request = new HttpRequestMessage(
+                HttpMethod.Post,
+                $"http://localhost:5130/{url}/{id}"
+            );
+
+            var response = await httpClient.SendAsync(request);
 
             response.EnsureSuccessStatusCode();
 
